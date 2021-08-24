c7801d459ece52b879961eb60a22d7824851cd04
KSanthanesh <santhanesh_2004@yahoo.com>
2021-08-24 13:38:04 +0000

    Add js for interests.html

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,34 +15,34 @@ function userInformationHTML(user) {
         </div>`;
 }
 
-
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
-    if(!username) {
-        $('#gh-user-data').html(`<h2>Please enter a Github Username</h2>`);
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
+
     $("#gh-user-data").html(
-        `<div id="loader")>
-        <img src="assets/css/ATB3o.gif" alt= "loading..."/>
+        `<div id="loader">
+            <img src="assets/css/ATB3o.gif" alt="loading..." />
         </div>`);
 
-$.when(
-    $.getJSON(`https://api.github.com/users/${username}`)
-).then(
-    function(response) {
-        var userData = response;
-        $("#gh-user-data").html(userInformationHTML(userData));
-    },
-    function(errorResponse) {
-        if (errorResponse.status === 404) {
-            $("#gh-user-data").html(
-                `<h2>No info found for user ${username}</h2>`);
-        } else {
-            console.log(errorResponse);
-            $("#gh-user-data").html(
-                `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-        }
-    });
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
